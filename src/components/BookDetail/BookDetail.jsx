846513550c1955298cb70fe469a59bdd032669a9
Aytacrustemli <aytacrustmli93@gmail.com>
2022-12-22 00:03:31 +0400

    read more added

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -11,6 +11,7 @@ import "swiper/scss/navigation";
 import "swiper/scss";
 import "swiper/scss/pagination";
 import { Navigation, Scrollbar, A11y } from "swiper";
+import ReadMoreReact from "read-more-react";
 
 const colors = {
   orange: "#FFBA5A",
@@ -77,31 +78,40 @@ const BookDetail = () => {
                 <br />
                 <span>
                   <strong>Yazar:</strong> adi
-                </span> <br />
+                </span>{" "}
+                <br />
                 <span>
                   <strong>Tercumeci :</strong> adi
-                </span> <br />
+                </span>{" "}
+                <br />
                 <span>
                   <strong>Sefiya sayi:</strong> 123
-                </span> <br />
+                </span>{" "}
+                <br />
                 <span>
                   <strong>Yayin tarixi :</strong> tarix
-                </span> <br />
+                </span>{" "}
+                <br />
                 <span>
                   <strong>Kitabin dili :</strong> dil
-                </span> <br />
+                </span>{" "}
+                <br />
                 <span>
                   <strong>Cild tipi:</strong> tip
-                </span> <br />
+                </span>{" "}
+                <br />
                 <span>
                   <strong>Kagiz cinsi :</strong> cinsi
-                </span> <br />
+                </span>{" "}
+                <br />
                 <span>
                   <strong>Olculer :</strong> 124
-                </span> <br />
+                </span>{" "}
+                <br />
                 <span>
                   <strong>Kategoryi:</strong> adi
-                </span> <br />
+                </span>{" "}
+                <br />
               </div>
               <p>orta rating cixsin ulduzla</p>
               <p className="stock">
@@ -236,9 +246,16 @@ const BookDetail = () => {
               </TabPanel>
               <TabPanel value="3">
                 <div id="description">
-                  <p>
-                    yazar haqqinfa melumat daha coxa basson oxusun
-                  </p>
+                  <p>yazar haqqinfa melumat daha coxa basson oxusun</p>
+                  <ReadMoreReact
+                    text={
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+                    }
+                    min={50}
+                    ideal={100}
+                    max={200}
+                    readMoreText={"read more"}
+                  />
                 </div>
               </TabPanel>
             </TabContext>
@@ -246,9 +263,7 @@ const BookDetail = () => {
         </div>
         <div className="similar">
           <div className="title">
-            <h4>
-              Yazarın digər kitabları
-            </h4>
+            <h4>Yazarın digər kitabları</h4>
           </div>
           <div className="books">
             <div className="bottom">
